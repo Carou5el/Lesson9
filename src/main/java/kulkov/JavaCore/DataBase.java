f4b07d1c3d0fbfabcb4fe7f2ec3e28f7617a68d7
Carou5el <garrydeepcool@outlook.com>
2021-02-12 21:48:46 +0500

    Adding a 'lesson.db'.

--- src/main/java/kulkov/JavaCore/DataBase.java
+++ src/main/java/kulkov/JavaCore/DataBase.java
@@ -2,15 +2,16 @@ package kulkov.JavaCore;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataBase {
 	private static Connection connection;
 	
-	private static void connect()   {
+	public static void connect()   {
 		try {
 			Class.forName("org.sqlite.JDBC");
-			connection = DriverManager.getConnection();
-		} catch (ClassNotFoundException e) {
+			connection = DriverManager.getConnection("jdbc:sqlite:lesson.db");
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
--- src/main/java/kulkov/JavaCore/Main.java
+++ src/main/java/kulkov/JavaCore/Main.java
@@ -1,7 +1,10 @@
 package kulkov.JavaCore;
 
+import javax.xml.crypto.Data;
+
 public class Main {
 	public static void main(String[] args) {
-	
+		DataBase db = new DataBase();
+		DataBase.connect();
 	}
 }
